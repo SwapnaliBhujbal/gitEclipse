97f023ea36e9848ce6dde6bc1fafe39a396c4601
Swapnali Bhujbal <swapnalibhujbal596@gmail.com>
2023-06-10 17:21:28 +0530

    second changes

--- MyGitProject/src/Mypackage/MyEclipseGitProject.java
+++ MyGitProject/src/Mypackage/MyEclipseGitProject.java
@@ -1,12 +1,16 @@
 package Mypackage;
 
 public class MyEclipseGitProject {
-
+	public static int sum(int a, int b) {
+		int c;
+		c = a + b;
+		return c;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello World");
-		System.out.println("Hello");
+		System.out.println(sum(5,3));
 
 	}
 
